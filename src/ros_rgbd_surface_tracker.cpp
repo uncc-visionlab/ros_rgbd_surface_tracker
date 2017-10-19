6377b70b48a205dfeaab9a66a39ad1435f7544f1
John Papadakis <jpapadak@uncc.edu>
2017-10-19 15:22:41 -0400

    Small fix in convertPoseToTFTransform. Pose tracking in rViz now correct.

--- src/ros_rgbd_surface_tracker.cpp
+++ src/ros_rgbd_surface_tracker.cpp
@@ -94,7 +94,6 @@ tf::Transform convertPoseToTFTransform(const Pose& pose) {
     Eigen::Map<Eigen::Matrix3f> eigenRot((float *) rotation.data);
     //std::cout << "rotation = " << rotation << std::endl;
     eigenRot.transposeInPlace();
-    translation = -translation;
     //std::cout << "eigenRot = " << eigenRot << std::endl;
     Eigen::Quaternionf quat(eigenRot);
     return tf::Transform(tf::Quaternion(quat.x(), quat.y(), quat.z(), quat.w()),
